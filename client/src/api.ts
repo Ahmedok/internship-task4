64c49b78b45bde62ccec69d6847547a846e36d8c
Assylzhan Sarinov (ahmedok) <a_sarinov@mail.ru>
2026-01-30 11:35:32 +0500

    feat: Quick interceptor added

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { type AxiosError } from 'axios';
 
 const api = axios.create({
     baseURL: 'http://localhost:3000/api',
@@ -11,5 +11,17 @@ api.interceptors.request.use((config) => {
     }
     return config;
 });
+
+api.interceptors.response.use(
+    (response) => response, // Success
+    (error: AxiosError) => {
+        // If user is no longer with us...
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            localStorage.clear();
+            window.location.href = '/login';
+        }
+        return Promise.reject(error);
+    },
+);
 
 export default api;
